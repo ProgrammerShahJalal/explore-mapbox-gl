1ad946b57b41aafc085785b16d1187566b745c1f
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-21 16:41:11 +0600

    mapbox

---
+++ src/components/MapDirection.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFyYWJpNDciLCJhIjoiY2tuZGgxZWpuMTNwYzJxbWlsbGpsbWUzdyJ9.w3jsLYOVX4UsDSVV-2nfvA';
+
+const MapDirection = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [90.389099, 23.791599],
+            zoom: 13
+        });
+
+        map.addControl(
+            new MapboxDirections({
+                accessToken: mapboxgl.accessToken
+            }),
+            'top-left'
+        );
+    }, [])
+    return (
+        <div>
+            <div id="map"></div>
+        </div>
+    );
+};
+
+export default MapDirection;
